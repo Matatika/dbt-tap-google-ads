39b102657a26997da78bfb64fb220997426c8cfb
danielpdwalker <danielpdwalker@gmail.com>
2021-12-01 10:22:33 +0000

    Fixed drop tables macro

--- macros/drop_google_ads_stream_tables.sql
+++ macros/drop_google_ads_stream_tables.sql
@@ -5,17 +5,18 @@
 -- Dropping these tables are an easy way to ensure that the 
 {%- macro drop_google_ads_stream_tables() -%}
     {%- set drop_query -%}
-        DROP TABLE {{ target.schema }}.stream_accessible_customers
-        DROP TABLE {{ target.schema }}.stream_adgroups
-        DROP TABLE {{ target.schema }}.stream_adgroupsperformance
-        DROP TABLE {{ target.schema }}.stream_campaign
-        DROP TABLE {{ target.schema }}.stream_campaign_performance
-        DROP TABLE {{ target.schema }}.stream_campaign_performance_by_age_range_and_device
-        DROP TABLE {{ target.schema }}.stream_campaign_performance_by_gender_and_device
-        DROP TABLE {{ target.schema }}.stream_campaign_performance_by_location
-        DROP TABLE {{ target.schema }}.stream_customer_hierarchystream
-        DROP TABLE {{ target.schema }}.stream_customers
-        DROP TABLE {{ target.schema }}.stream_geo_target_constant
+        DROP TABLE 
+        {{ target.schema }}.stream_accessible_customers
+        , {{ target.schema }}.stream_adgroups
+        , {{ target.schema }}.stream_adgroupsperformance
+        , {{ target.schema }}.stream_campaign
+        , {{ target.schema }}.stream_campaign_performance
+        , {{ target.schema }}.stream_campaign_performance_by_age_range_and_device
+        , {{ target.schema }}.stream_campaign_performance_by_gender_and_device
+        , {{ target.schema }}.stream_campaign_performance_by_location
+        , {{ target.schema }}.stream_customer_hierarchystream
+        , {{ target.schema }}.stream_customers
+        , {{ target.schema }}.stream_geo_target_constant
     {%- endset -%}
     {% do run_query(drop_query) %}
 {%- endmacro -%}
